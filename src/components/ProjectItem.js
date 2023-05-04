c2dda967e70733d1de429fbf63ef53e0f1e2987e
Zoe Guidry <oyezoey@gmail.com>
2023-05-04 12:28:05 -0500

    lab completed

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,7 +6,11 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologies.map(tech => {
+          return (
+            <span key={tech}>{tech}</span>
+          )
+        })}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,13 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map(project => {
+          return (
+            <ProjectItem {...project} key={project.id} />
+          )
+        })}
+      </div>
     </div>
   );
 }
